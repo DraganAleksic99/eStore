6f687880d6dfa1f5f09698ad729d3c916692a13d
Dragan Aleksic <aleksicdragan574@gmail.com>
2023-11-14 23:20:07 +0100

    .

--- src/shop/routes.tsx
+++ src/shop/routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { NotFound } from "./NotFound"
 import { Shop } from "./Shop"
 import { CartDetails } from "./CartDetails"
@@ -8,7 +8,8 @@ import { Thanks } from "./Thanks"
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Shop />} >
+            <Route path="/" element={<Navigate to="/shop/products"/>} />
+            <Route path="shop/products" element={<Shop />} >
                 <Route path=":category" element={<Shop />} />
             </Route>
             <Route path="shop/cart" element={<CartDetails />} />
