ae99e4db6e19a3b234c5a194cccda16e7820216e
Dragan Aleksic <aleksicdragan574@gmail.com>
2023-11-18 13:01:39 +0100

    .

--- src/shop/Thanks.tsx
+++ src/shop/Thanks.tsx
@@ -1,10 +1,6 @@
 import { Link } from 'react-router-dom'
-import { useGetOrdersQuery } from '../modules/orders/ordersApi'
 
 export const Thanks = () => {
-  const { data: orders } = useGetOrdersQuery({})
-  console.log(orders)
-
   return (
     <div>
       <div className="col bg-dark text-white">
@@ -12,7 +8,6 @@ export const Thanks = () => {
       </div>
       <div className="m-2 text-center">
         <h2>Thanks!</h2>
-        <h2>{orders}</h2>
         <p>Thanks for placing your order.</p>
         <p>We'll ship your goods as soon as possible.</p>
         <Link to="/shop/products" className="btn btn-primary">
